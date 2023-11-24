23ba03750f4a0e0ed697acdc74b63a0f8cebbe43
nguyen thanh long <loglog050801>
2023-11-24 17:08:50 +0700

    update publishes view

--- src/FileManagerServiceProvider.php
+++ src/FileManagerServiceProvider.php
@@ -11,8 +11,11 @@ class FileManagerServiceProvider extends ServiceProvider
         $this->loadRoutesFrom(__DIR__.'/routes/web.php');
         $this->loadMigrationsFrom(__DIR__.'/database/migrations');
         $this->loadViewsFrom(__DIR__.'/resources/views','file-manager');
+//        $this->publishes([
+//            __DIR__ . '/../resources/views' => resource_path('views/vendor/application-manager'),
+//        ], 'views');
         $this->publishes([
-            __DIR__.'/resources/views' => base_path('resources/views/'),
+            __DIR__.'/resources/views' => resource_path('views/vendor/file-manager'),
             __DIR__ . '/config/file-manager.php' => config_path('file-manager.php'),
             __DIR__.'/public/css' => public_path('vendor/file-manager/css'),
             __DIR__.'/public/js' => public_path('vendor/file-manager/js'),

2fcc206ec79eff09d0c967e16141e5c84580dda5
nguyen thanh long <loglog050801>
2023-11-27 14:39:02 +0700

    update service provider

--- src/FileManagerServiceProvider.php
+++ src/FileManagerServiceProvider.php
@@ -8,22 +8,23 @@ class FileManagerServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        $this->loadRoutesFrom(__DIR__.'/routes/web.php');
-        $this->loadMigrationsFrom(__DIR__.'/database/migrations');
-        $this->loadViewsFrom(__DIR__.'/resources/views','file-manager');
+        if ($this->app->runningInConsole()) {
+            $this->loadRoutesFrom(__DIR__ . '/routes/web.php');
+            $this->loadMigrationsFrom(__DIR__ . '/database/migrations');
+            $this->loadViewsFrom(__DIR__ . '/resources/views', 'file-manager');
 
-        $this->publishes([
-            __DIR__.'/resources/views' => resource_path('views/vendor/file-manager'),
-            __DIR__ . '/config/file-manager.php' => config_path('file-manager.php'),
-            __DIR__.'/public/css' => public_path('vendor/file-manager/css'),
-            __DIR__.'/public/js' => public_path('vendor/file-manager/js'),
-            __DIR__.'/public/icons' => public_path('vendor/file-manager/icons'),
-            __DIR__ . '/public/images' => public_path('vendor/file-manager/images'),
-        ], 'file-manager');
+            $this->publishes([
+                __DIR__ . '/resources/views' => resource_path('views/vendor/file-manager'),
+                __DIR__ . '/config/file-manager.php' => config_path('file-manager.php'),
+                __DIR__ . '/public/css' => public_path('vendor/file-manager/css'),
+                __DIR__ . '/public/js' => public_path('vendor/file-manager/js'),
+                __DIR__ . '/public/icons' => public_path('vendor/file-manager/icons'),
+                __DIR__ . '/public/images' => public_path('vendor/file-manager/images'),
+            ], 'file-manager');
+        }
     }
 
     public function register()
     {
-
     }
 }
